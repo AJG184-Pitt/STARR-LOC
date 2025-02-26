f4e7bc2fe4b7bd671ea8a903d9702c3ceb6930aa
Aidan Gresko <AJG184@pitt.edu>
2025-02-26 14:43:24 -0500

    completed for now - serial

--- Serial/Serial/Serial.ino
+++ Serial/Serial/Serial.ino
@@ -1,11 +1,34 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float temp = Serial.read();
-  Serial.print(temp);
-  // Serial.write(temp);
+  // Check if data is available
+  if (Serial.available() > 0) {
+    // Wait a bit for the entire message to arrive
+    delay(100);
+    
+    // Read the whole string
+    String inputString = "";
+    while (Serial.available() > 0) {
+      char inChar = (char)Serial.read();
+      inputString += inChar;
+      delay(50);
+    }
+    
+    // Parse the string for x and y values
+    int spaceIndex = inputString.indexOf(' ');
+    if (spaceIndex != -1) {
+      float x = inputString.substring(0, spaceIndex).toFloat();
+      float y = inputString.substring(spaceIndex + 1).toFloat();
+      
+      // Echo back what was received
+      Serial.print("Received x: ");
+      Serial.print(x, 5);
+      Serial.print(", y: ");
+      Serial.println(y, 5);
+    } else {
+      Serial.println("Error: Expected format 'x y'");
+    }
+  }
 }
