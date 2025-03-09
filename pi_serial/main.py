587af45237ed2573962a81d2aee21c539d9371fb
Aidan Gresko <AJG184@pitt.edu>
2025-03-09 01:02:55 -0500

    some changes

--- pi_serial/main.py
+++ pi_serial/main.py
@@ -1,13 +1,13 @@
 import serial
 import time
 
-ser = serial.Serial('dev/tty/ACM0', 9600, timeout=1)
-time.sleep(2)
+ser = serial.Serial('dev/ttyACM0', 9600, timeout=1)
+time.sleep(1)
 
 data = "3.5 4.7"
 
 ser.write(data.encode())
 
-time.sleep(2)
+time.sleep(1)
 
 ser.close()
