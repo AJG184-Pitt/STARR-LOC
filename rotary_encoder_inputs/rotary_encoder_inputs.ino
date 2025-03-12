fccba9a897b6e46a8cb6860da9932649a4356503
Aidan Gresko <AJG184@pitt.edu>
2025-03-12 18:56:48 -0400

    changes to user inputs

--- rotary_encoder_inputs/rotary_encoder_inputs.ino
+++ rotary_encoder_inputs/rotary_encoder_inputs.ino
@@ -1,5 +1,4 @@
 #include <Stepper.h>
-
 /*==========================================================================
 // Author : Handson Technology
 // Project : Arduino Uno with KY040 Rotary Encoder Module
@@ -11,50 +10,46 @@
 int pinA = 2; // Connected to CLK on KY-040
 int pinB = 3; // Connected to DT on KY-040
 int encoderPosCount = 0;
-int pinALast;
-int aVal;
-boolean bCW;
+boolean direction;
 
-// Need to multiply steps by 64 due to the gear ratio
-Stepper stepper(32, 8,9,10,11);
+Stepper stepper(2048, 4, 6, 5, 7);
 
 void setup() {
-  pinMode (pinA, INPUT_PULLUP);
-  pinMode (pinB, INPUT_PULLUP);
-  stepper.setSpeed(200);
-  /* Read Pin A
-  Whatever state it's in will reflect the last position
-  */
-  pinALast = digitalRead(pinA);
-  Serial.begin (9600);
+  pinMode(pinA, INPUT_PULLUP);
+  pinMode(pinB, INPUT_PULLUP);
+
+  stepper.setSpeed(10);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  aVal = digitalRead(pinA);
-  // int val = 0
-  if (aVal != pinALast) { // Means the knob is rotating
-    // if the knob is rotating, we need to determine direction
-    // We do that by reading pin B.
-    if (digitalRead(pinB) != aVal) { // Means pin A Changed first - We're Rotating Clockwise.
-      encoderPosCount++;
-      bCW = true;
-      stepper.step(3.2*64);
-    } else {// Otherwise B changed first and we're moving CCW
-      bCW = false;
-      encoderPosCount--;
-      stepper.step(-3.2*64);
+  static int pinALast = digitalRead(pinA);
+  
+  // Read current state of A and B pins
+  int aVal = digitalRead(pinA);
+  int bVal = digitalRead(pinB);
+
+  if (aVal != pinALast) { // Check for transitions on A
+    if (bVal == aVal) {
+      direction = true;   // Clockwise rotation detected
+      stepper.step(57);     // Take one step in the correct direction
+    } else {
+      direction = false;  // Counter-clockwise rotation detected
+      stepper.step(-57);   // Take one step in the opposite direction
     }
     
-    Serial.print ("Rotated: ");
-    if (bCW){
-      Serial.println ("clockwise");
-    }else{
+    pinALast = aVal;
+  
+    Serial.print("Rotated: ");
+    if (direction) {
+      Serial.println("clockwise");
+    } else {
       Serial.println("counterclockwise");
     }
-  
+
+    encoderPosCount += direction ? 1 : -1; // Update position counter based on direction
     Serial.print("Encoder Position: ");
     Serial.println(encoderPosCount);
   }
-  
-  pinALast = aVal;
 }
