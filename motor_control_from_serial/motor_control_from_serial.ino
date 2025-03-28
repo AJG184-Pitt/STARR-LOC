0f6cf41db2b8f321a46a332b3df692349c9ccb42
arnic52 <122462093+arnic52@users.noreply.github.com>
2025-03-27 23:17:59 -0400

    working code for taking steps for az and el from serial and moving by that amount after calibration

--- motor_control_from_serial/motor_control_from_serial.ino
+++ motor_control_from_serial/motor_control_from_serial.ino
@@ -8,6 +8,9 @@ const uint8_t az_dir_pin = 4;
 const uint8_t el_step = 3;
 const uint8_t el_dir_pin = 2;
 
+// number of steps in a revolution
+const float steps_per_revolution = 4450;
+
 /* Mpu9250 object, SPI bus, CS on pin 10 */
 bfs::Mpu9250 imu(&SPI, 10);
 
@@ -42,7 +45,28 @@ void setup() {
   }
 
 
+  level();
+  north();
 
+  while(1){
+    // wait for user input
+    Serial.print("\nInput azimuth steps:\n");
+    while(Serial.available() == 0){}
+    String user_input = Serial.readString();
+    float az_steps = user_input.toFloat();
+    bool az_dir = az_steps < 0;
+    uint16_t az_steps_abs = (uint16_t)round(abs(az_steps));
+
+    Serial.print("Input elevation steps:\n");
+    while(Serial.available() == 0){}
+    user_input = Serial.readString();
+    float el_steps = user_input.toFloat();
+    bool el_dir = el_steps < 0;
+    uint16_t el_steps_abs = (uint16_t)round(abs(el_steps));
+
+    // move
+    azel_step_control(az_dir,az_steps_abs,el_dir,el_steps_abs);
+  }
 
   while(1){}
 }
@@ -81,6 +105,43 @@ void el_step_control(bool dir, uint16_t num_steps){
   }
 }
 
+// move both az and el at same time, finish by moving just along longer measurement
+void azel_step_control(bool az_dir, uint16_t az_steps, bool el_dir, uint16_t el_steps){
+
+  // set direction of rotation
+  if(az_dir){ digitalWrite(az_dir_pin, LOW);}
+  else{ digitalWrite(az_dir_pin, HIGH);}
+
+  // set direction of rotation
+  if(el_dir){ digitalWrite(el_dir_pin, LOW);}
+  else{ digitalWrite(el_dir_pin, HIGH); }
+
+  // handle stepping a certain number of steps
+  uint16_t az_step_curr = az_steps;
+  uint16_t el_step_curr = el_steps;
+  Serial.print("AZ: ");
+  Serial.print(az_step_curr);
+  Serial.print("\tEL: ");
+  Serial.print(el_step_curr);
+  Serial.print("\n");
+  
+  while ((az_step_curr > 0) & (el_step_curr > 0)){
+    digitalWrite(az_step, HIGH);
+    digitalWrite(el_step, HIGH);
+    delay(1);
+    digitalWrite(az_step, LOW);
+    digitalWrite(el_step, LOW);
+    delay(1);
+    az_step_curr--;
+    el_step_curr--;
+  }
+
+  // finish the rest of the motion on one axis
+  if(az_step_curr > 0){ az_step_control(az_dir, az_step_curr);}
+  if(el_step_curr > 0){ el_step_control(el_dir, el_step_curr);}
+
+}
+
 // when level, get the current azimuth measurement
 float get_az(){
 
@@ -108,7 +169,7 @@ float get_az(){
     mag_y = (mag_y - (-198.96))/26.5;
 
     // get the elevation from the atan function
-    float az_meas = atan(mag_x/mag_y);
+    float az_meas = atan(mag_x/mag_y)*180/3.1415 + 40;
 
     return az_meas;
 }
@@ -127,14 +188,14 @@ float get_el(){
     while(i < num_avg){
       if(imu.Read()){
         accel_x += imu.accel_x_mps2()/num_avg;
-        accel_y += imu.accel_x_mps2()/num_avg;
-        accel_z += imu.accel_x_mps2()/num_avg;
+        accel_y += imu.accel_y_mps2()/num_avg;
+        accel_z += imu.accel_z_mps2()/num_avg;
         i++;
       }
     }
 
     // get the elevation from the atan function
-    float el_meas = atan(accel_x/accel_z);
+    float el_meas = atan(accel_x/accel_z)*-180/3.1415;
 
     return el_meas;
 }
@@ -146,7 +207,20 @@ void level(){
   float el_curr = get_el();
 
   // correct for the error in elevation
-  bool el_dir = el_curr > 0;
-  int16_t el_steps_abs = (int16_t)round(abs())
+  bool el_dir = el_curr < 0;
+  int16_t el_steps_abs = (int16_t)round(abs(el_curr/360*steps_per_revolution));
+  if(el_steps_abs > 5){ el_step_control(el_dir, el_steps_abs);}
+}
+
+// find north when level
+void north(){
+
+  // get the current azimuth angle
+  float az_curr = get_az();
+
+  // correct for the error in elevation
+  bool az_dir = az_curr > 0;
+  int16_t az_steps_abs = (int16_t)round(abs(az_curr/360*steps_per_revolution));
+  if(az_steps_abs > 5){ az_step_control(az_dir, az_steps_abs);}
 }
 
