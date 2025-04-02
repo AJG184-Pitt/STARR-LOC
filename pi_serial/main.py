6eb052b30ed1cb15b338583dcadb1f310dec3873
AJG184-Pitt <AJG184@pitt.edu>
2025-04-02 14:07:23 -0400

    newest

--- pi_serial/main.py
+++ pi_serial/main.py
@@ -2,15 +2,23 @@ from serial import Serial
 import time
 
 ser = Serial('/dev/ttyACM0', 115200, timeout=1)
-time.sleep(20)
+time.sleep(10)
 
-data1 = "30\n"
+data1 = "100 100\n"
 ser.write(data1.encode())
+time.sleep(2)
 
-time.sleep(1)
+data1 = "200 0\n"
+ser.write(data1.encode())
+time.sleep(2)
 
-data2 = "30\n"
-ser.write(data2.encode)
+data1 = "0 200\n"
+ser.write(data1.encode())
+time.sleep(2)
+
+data1 = "-300 -300\n"
+ser.write(data1.encode())
+time.sleep(2)
 
 time.sleep(1)
 
