7c76a7eebaf2edb473c3df45c2492b274cecd7a7
arnic52 <arnichols810@gmail.com>
2025-04-12 21:37:29 -0400

    change to test carrier freq

--- UI-pyqt6/main.py
+++ UI-pyqt6/main.py
@@ -469,7 +469,7 @@ class MainWindow(QMainWindow):
                   #                 stderr=None)
 
                 
-                self.radio_process = subprocess.Popen(['/path/to/gnuradio.sh', f"{sat_freq[self.combo_box.currentIndex().name]}"], 
+                self.radio_process = subprocess.Popen(['/path/to/gnuradio.sh', f"{147540000}"], 
                                    stdin=None,
                                    stdout=None,
                                    stderr=None)
