0d1315298c90806f977246250ad4a9473952fbf8
arnic52 <arnichols810@gmail.com>
2025-04-12 23:32:16 -0400

    add bash for gnuradio

---
+++ UI-pyqt6/gnuradio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+ARG="$2"
+
+source "/home/starrloc/miniconda3/etc/profile.d/conda.sh"
+conda activate base
+
+python3 "/home/starrloc/Documents/STARR-LOC/radio/GNU Radio/Autocorrelation Voice Squelch/HAM/fm_rx.py" "$ARG"
