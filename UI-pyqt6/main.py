9db16a2e7bc0a72ff1a3bc453b45b16f5052a753
Thomas Eckrich <103239931+Teckrich@users.noreply.github.com>
2025-04-17 16:11:03 -0400

    timezone again

--- UI-pyqt6/main.py
+++ UI-pyqt6/main.py
@@ -1055,7 +1055,7 @@ class MainWindow(QMainWindow):
 
         self.demo_mode = True
         #et = datetime.timezone("US/Eastern")
-        local_time = datetime.datetime(2025, 4, 17, 16, 25, 20, tzinfo=pytz.timezone("US/Eastern"))
+        local_time = datetime.datetime(2025, 4, 17, 16, 25, 20)
         local_time = local_time.astimezone(pytz.utc)
 
         sat = next((sat for sat in self.satellites if sat.name == "AO-73"), None)
